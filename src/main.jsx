5ce1185459eced67c01213c969219008e0e440e8
Kavana-DR <kavanadr45@gmail.com>
2026-02-17 15:35:12 +0530

    Setup design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+import PremiumLayout from './rb/PremiumLayout.jsx'
+import StepPage from './rb/StepPage.jsx'
+import ProofPage from './rb/Proof.jsx'
+
+function AppRoutes() {
+  const base = '/rb'
+  const steps = [
+    '01-problem',
+    '02-market',
+    '03-architecture',
+    '04-hld',
+    '05-lld',
+    '06-build',
+    '07-test',
+    '08-ship',
+  ]
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/rb/01-problem" replace />} />
+        <Route path="/rb" element={<Navigate to="/rb/01-problem" replace />} />
+        <Route path="/rb/proof" element={<PremiumLayout><ProofPage /></PremiumLayout>} />
+        {steps.map((p, idx) => (
+          <Route
+            key={p}
+            path={`/rb/${p}`}
+            element={<PremiumLayout><StepPage step={idx + 1} slug={p} /></PremiumLayout>}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>,
 )
